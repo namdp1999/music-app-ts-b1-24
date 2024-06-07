dddd2324819c0667622233ebdec1f020c8cdedd6
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-07 20:45:29 +0700

    2.2. Tạo route cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.send("Chủ đề bài hát");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
