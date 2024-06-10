eb95384119dec740aa744f52283b2946facc9f35
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-10 19:18:25 +0700

    3.3. Cấu trúc lại folder PUG

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -9,5 +9,7 @@ export const index = async (req: Request, res: Response) => {
 
   console.log(topics);
 
-  res.render("client/pages/topics/index");
+  res.render("client/pages/topics/index", {
+    pageTitle: "Chủ đề bài hát"
+  });
 }
