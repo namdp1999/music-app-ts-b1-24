4023efb49c7bb17d02faae92457537b756b47c0b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-10 19:12:35 +0700

    3.2. Tạo controllers

---
+++ controllers/client/topic.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+
+// [GET] /topics/
+export const index = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+
+  console.log(topics);
+
+  res.render("client/pages/topics/index");
+}
--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,16 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 const router: Router = Router();
 
-import Topic from "../../models/topic.model";
+import * as controller from "../../controllers/client/topic.controller";
 
-router.get("/", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+router.get("/", controller.index);
 
 export const topicRoutes: Router = router;
