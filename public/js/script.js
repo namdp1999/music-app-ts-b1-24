228cc5135eca2904712a0e2810ff562b7837c76c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-10 20:44:24 +0700

    1.1. Làm trang chi tiết bài hát - Tích hợp APlayer

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
-console.log("OK");
+// APlayer
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  audio: [{
+      name: 'Cắt đôi nỗi sầu',
+      artist: 'Tăng Duy Tân',
+      url: 'https://backend.daca.vn/assets/audios/cat-doi-noi-sau.mp3',
+      cover: 'https://backend.daca.vn/assets/images/cat-doi-noi-sau.jpg'
+  }]
+});
+// End APlayer
